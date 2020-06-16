0947e876fdc79d540d45c1130e6f9ca519816e49
HerrBaumx <731278859@qq.com>
2020-06-16 22:39:21 +0800

    服务端菜单接口设计

--- src/main/java/org/jxiao/vhrself/mapper/MenuMapper.java
+++ src/main/java/org/jxiao/vhrself/mapper/MenuMapper.java
@@ -2,6 +2,8 @@ package org.jxiao.vhrself.mapper;
 
 import org.jxiao.vhrself.model.Menu;
 
+import java.util.List;
+
 public interface MenuMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -14,4 +16,6 @@ public interface MenuMapper {
     int updateByPrimaryKeySelective(Menu record);
 
     int updateByPrimaryKey(Menu record);
+
+    List<Menu> getMenuByHrId(Integer hrid);
 }
---
+++ src/main/java/org/jxiao/vhrself/serice/MenuService.java
@@ -0,0 +1,23 @@
+package org.jxiao.vhrself.serice;
+
+import org.jxiao.vhrself.mapper.MenuMapper;
+import org.jxiao.vhrself.model.Hr;
+import org.jxiao.vhrself.model.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import sun.plugin2.message.GetAuthenticationMessage;
+
+import java.util.List;
+
+@Service
+public class MenuService {
+    @Autowired
+    MenuMapper menuMapper;
+
+    public List<Menu> getMenuByHrId() {
+
+        return menuMapper.getMenuByHrId(((Hr)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId());
+    }
+}
