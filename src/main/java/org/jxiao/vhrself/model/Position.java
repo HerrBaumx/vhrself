89e1f628c735549c3bcc8e7edf4e849cca3e984b
HerrBaumx <731278859@qq.com>
2020-06-29 22:30:38 +0800

    职位管理后端统一异常处理时间格式

--- src/main/java/org/jxiao/vhrself/model/Position.java
+++ src/main/java/org/jxiao/vhrself/model/Position.java
@@ -1,5 +1,7 @@
 package org.jxiao.vhrself.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public class Position {
@@ -7,6 +9,7 @@ public class Position {
 
     private String name;
 
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss", timezone = "Asia/Shanghai")
     private Date createDate;
 
     private Boolean enabled;
