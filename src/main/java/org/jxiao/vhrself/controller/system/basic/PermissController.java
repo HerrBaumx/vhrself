dca9e6d0deab471f464873438591bdbb0780f286
HerrBaumx <731278859@qq.com>
2020-07-02 20:38:47 +0800

    权限组菜单角色关系修改

--- src/main/java/org/jxiao/vhrself/controller/system/basic/PermissController.java
+++ src/main/java/org/jxiao/vhrself/controller/system/basic/PermissController.java
@@ -1,13 +1,12 @@
 package org.jxiao.vhrself.controller.system.basic;
 
 import org.jxiao.vhrself.model.Menu;
+import org.jxiao.vhrself.model.RespBean;
 import org.jxiao.vhrself.model.Role;
 import org.jxiao.vhrself.serice.MenuService;
 import org.jxiao.vhrself.serice.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +27,19 @@ public class PermissController {
     @GetMapping("/menus")
     public List<Menu> getAllMenus(){
         return menuService.getAllMenus();
+    }
+
+    @GetMapping("/mids/{rid}")
+    public List<Integer> getMidsByRid(@PathVariable Integer rid) {
+        return menuService.getMidsByRid(rid);
+
+    }
+
+    @PutMapping("/")
+    public RespBean updateMenuRole(Integer rid, Integer[] mids) {
+        if (menuService.updateMenuRole(rid, mids)) {
+            return RespBean.ok("跟新成功！");
+        }
+        return RespBean.error("跟新失败！");
     }
 }
--- src/main/java/org/jxiao/vhrself/serice/MenuService.java
+++ src/main/java/org/jxiao/vhrself/serice/MenuService.java
@@ -1,13 +1,13 @@
 package org.jxiao.vhrself.serice;
 
 import org.jxiao.vhrself.mapper.MenuMapper;
+import org.jxiao.vhrself.mapper.MenuRoleMapper;
 import org.jxiao.vhrself.model.Hr;
 import org.jxiao.vhrself.model.Menu;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import sun.plugin2.message.GetAuthenticationMessage;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,16 +16,32 @@ public class MenuService {
     @Autowired
     MenuMapper menuMapper;
 
+    @Autowired
+    MenuRoleMapper menuRoleMapper;
+
     public List<Menu> getMenuByHrId() {
 
-        return menuMapper.getMenuByHrId(((Hr)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId());
+        return menuMapper.getMenuByHrId(((Hr) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId());
     }
 
-    public List<Menu> getAllMenusWithRole(){
+    public List<Menu> getAllMenusWithRole() {
         return menuMapper.getAllMenusWithRole();
     }
 
     public List<Menu> getAllMenus() {
         return menuMapper.getAllMenus();
+    }
+
+    public List<Integer> getMidsByRid(Integer rid) {
+        return menuMapper.getMidsByRid(rid);
+    }
+
+    @Transactional
+    public boolean updateMenuRole(Integer rid, Integer[] mids) {
+
+        menuRoleMapper.deleteByRid(rid);
+        Integer result = menuRoleMapper.insertRecord(rid, mids);
+
+        return result == mids.length;
     }
 }
